71ee14551a425e3db3bbe5d0e969693248ac3706
Hy-ta <hyuuga.1.29.59@gamil.com>
2020-02-15 10:15:09 +0900

    devise user add

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "home#index"
+
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+
+  devise_scope :user do
+    get "users/show", :to => "users/user_id#show"
+    get "user/id", :to => "users/registration#detail"
+    get "signup", :to => "users/registrations#new"
+    get "sign_in", :to => "devise/sessions#new"
+    get "login", :to => "users/sessions#new"
+  end
+  
+  root to: 'pages#index'
 end
