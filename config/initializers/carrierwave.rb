799eaff8b2b3bc19c7f952005bd816ca2fbf857e
Hy-ta <hyuuga.1.29.59@gamil.com>
2020-02-25 22:19:21 +0900

    carrier wave file fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 unless Rails.env.development? || Rails.env.test?
     CarrierWave.configure do |config|
-        config.fig_credentials = {
+        config.fog_credentials= {
             provider: 'AWS',
             aws_access_key_id: ENV['aws_access_key_id'],
             aws_secret_access_key: ENV['aws_secret_access_key'],
