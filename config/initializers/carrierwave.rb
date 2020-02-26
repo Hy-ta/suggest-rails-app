044c3f0a432a935d478b71e3930bc3a1bcb418b2
Hy-ta <hyuuga.1.29.59@gamil.com>
2020-02-26 09:40:46 +0900

    s3 key setting

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,20 @@
-unless Rails.env.development? || Rails.env.test?
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+if Rails.env.production?
     CarrierWave.configure do |config|
-        config.fog_credentials= {
+        config.fog_provider = 'fog/aws'
+        config.fog_credentials = {
             provider: 'AWS',
             aws_access_key_id: ENV['aws_access_key_id'],
             aws_secret_access_key: ENV['aws_secret_access_key'],
             region: 'ap-northeast-1'
         }
 
-        config.fig_directory = 'rails-image-123'
-        config.asset_host = "https://s3.ap-northeast-1.amazonaws.com/suggest-com"
-        config.cache_storage = :fog
+        config.fog_directory = ENV['AWS_S3_BUCKET']
+        config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
     end
+
+    CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 end
