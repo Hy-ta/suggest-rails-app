66e06c3aad48958c49fa24b6a5b4535cd64b4ab4
Hy-ta <hyuuga.1.29.59@gamil.com>
2020-03-08 22:00:11 +0900

    top page edit

--- app/models/message.rb
+++
@@ -1,8 +0,0 @@
-class Message < ApplicationRecord
-  belongs_to :user
-  belongs_to :room
-  # after_create_commit { MessageBroadcastJob.perform_later self }
-
-  validates :content, presence: true
-  scope :recent, -> { order(created_at: :desc)}
-end
--- test/controllers/messages_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class MessagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get resources" do
-    get messages_resources_url
-    assert_response :success
-  end
-
-end
